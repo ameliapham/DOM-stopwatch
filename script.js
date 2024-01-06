13d4e12beee2d17372c58cb0fd6b153bd54089b2
ameliapham <ptt.huong.8895@gmail.com>
2024-01-06 22:07:39 +0100

    Variables for buttons

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 import ('./style.css')
+
+// Variables for buttons
+const startBtn = document.querySelector('#startBtn')
+const pauseBtn = document.querySelector('#pauseBtn')
+const resetBtn = document.querySelector('#resetBtn')
