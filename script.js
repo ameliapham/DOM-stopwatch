9825554d249df9d8a86765bb28bceac5320c6bd9
ameliapham <ptt.huong.8895@gmail.com>
2024-01-06 22:17:23 +0100

    Add stopwatch function

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const resetBtn = document.querySelector('#resetBtn')
 let seconds = 0
 let minutes = 0
 let hours = 0
+
+// Stopwatch function
+const stopWatch = () => {
+    seconds++
+
+    if (seconds/60 === 1){
+        seconds = 0
+        minutes++
+
+        if (minutes/60 === 1){
+            minutes = 0
+            hours++
+        }
+    }
+
+    let displayTimer = document.getElementById('#timer').innerText = hours + ":" + minutes + ":" + seconds
+}
