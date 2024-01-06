2701ac2ebc6ccbbd26e14d69c79991c1f79a23d6
ameliapham <ptt.huong.8895@gmail.com>
2024-01-06 22:09:33 +0100

    Variables for time values

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ import ('./style.css')
 const startBtn = document.querySelector('#startBtn')
 const pauseBtn = document.querySelector('#pauseBtn')
 const resetBtn = document.querySelector('#resetBtn')
+
+// Variables for time values 
+let seconds = 0
+let minutes = 0
+let hours = 0
